05aa42f08522e12492bc533df7231e6eb408ee7b
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-08-06 17:14:19 -0500

    feat: added validators.

---
+++ validators/tracks.js
@@ -0,0 +1,25 @@
+const { check } = require('express-validator');
+const validateResults = require('../utils/handleValidator');
+
+const validatorCreateItem = [
+    check('name').exists().notEmpty(),
+    check('album').exists().notEmpty(),
+    check('cover').exists().notEmpty(),
+    check('artist').exists().notEmpty(),
+    check('artist.name').exists().notEmpty(),
+    check('artist.nickname').exists().notEmpty(),
+    check('artist.nationality').exists().notEmpty(),
+    check('duration.start').exists().notEmpty(),
+    check('duration.end').exists().notEmpty(),
+    check('mediaId').exists().notEmpty().isMongoId(),
+    (req, res, next) => {
+        /* const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next(); */
+        return validateResults(req, res, next);
+    }
+];
+
+module.exports = { validatorCreateItem };
