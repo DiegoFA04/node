5dad58f74cf3802bc4a119d97e01b3f26f7b544d
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-08-09 14:45:34 -0500

    feat: added storage crud methods.

--- routes/storage.js
+++ routes/storage.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const uploadMiddleware = require('../utils/handleStorage');
-const { createItem } = require('../controllers/storage');
+const { getItems, getItem, createItem, updateItem, deleteItem } = require('../controllers/storage');
+const { validatorGetItem } = require('../validators/storage');
+
 //TODO: http://localhost:3000/api/storage
 
+/**
+ * Obtener lista de base de datos
+ */
+router.get('/', getItems);
 
+/**
+ * Obtener un item de la base de datos
+ */
+router.get('/:id', validatorGetItem, getItem);
 
+/**
+ * Crear un item en la base de datos
+ */
 router.post('/', uploadMiddleware.single('myfile'), createItem);
+
+/**
+ * Eliminar un item en la base de datos
+ */
+router.delete('/:id', validatorGetItem, deleteItem);
 
 module.exports = router;
