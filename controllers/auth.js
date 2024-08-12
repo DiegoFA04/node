ea0604b845977d01cbbd29cf2c14c219d7d80cc6
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-08-12 17:39:29 -0500

    feat: updated controllers.

---
+++ controllers/auth.js
@@ -0,0 +1,61 @@
+const { matchedData } = require('express-validator');
+const { encrypt, compare } = require('../utils/handlePassword');
+const { tokenSign } = require('../utils/handleJwt');
+const { handleHttpError } = require('../utils/handleError');
+const { usersModel } = require('../models');
+
+/**
+ * Este controlador recibe los datos del usuario y los guarda en la base de datos
+ * @param {*} req 
+ * @param {*} res 
+ */
+const registerCtrl = async (req, res) => {
+    try {
+        req = matchedData(req);
+        const password = await encrypt(req.password);
+        const body = { ...req, password };
+        const dataUser = await usersModel.create(body);
+        dataUser.set('password', undefined, { strict: false });
+        const data = {
+            token: await tokenSign(dataUser),
+            user: dataUser,
+        };
+        res.send({ data });
+    } catch (error) {
+        handleHttpError(res, 'Error al registrar el usuario');//403
+    }
+};
+
+/**
+ * Este controlador recibe los datos del usuario y verifica si el usuario existe en la base de datos
+ * @param {*} req 
+ * @param {*} res 
+ */
+const loginCtrl = async (req, res) => {
+    try {
+        req = matchedData(req);
+        const user = await usersModel.findOne({ email: req.email }).select('password name email role');
+        if (!user) {
+            handleHttpError(res, 'USER_NOT_EXISTS', 404);
+            return;
+        }
+
+        const hashPassword = user.password;
+        const check = await compare(req.password, hashPassword);
+        if (!check) {
+            handleHttpError(res, 'PASSWORD_INVALID', 401);
+            return;
+        }
+
+        user.set('password', undefined, { strict: false });
+        const data = {
+            token: await tokenSign(user),
+            user
+        };
+        res.send({ data });
+    } catch (error) {
+        handleHttpError(res, 'Error login user');//403
+    }
+}
+
+module.exports = { registerCtrl, loginCtrl };
--- controllers/storage.js
+++ controllers/storage.js
@@ -1,7 +1,10 @@
+const fs = require('fs');
 const { matchedData } = require('express-validator');
 const { storageModel } = require('../models');
 const { handleHttpError } = require('../utils/handleError');
+
 const PUBLIC_URL = process.env.PUBLIC_URL;
+const MEDIA_PATH = `${__dirname}/../storage`;
 
 /**
  * Obtener lista de base de datos
@@ -11,7 +14,7 @@ const PUBLIC_URL = process.env.PUBLIC_URL;
 const getItems = async (req, res) => {
     try {
         const data = await storageModel.find({});
-        res.send({ data: data });
+        res.send({ data });
     } catch (error) {
         handleHttpError(res, 'Error_list_item');//403
     }
@@ -26,7 +29,7 @@ const getItem = async (req, res) => {
     try {
         const { id } = matchedData(req);
         const data = await storageModel.findById(id);
-        res.send({ data: data });
+        res.send({ data });
     } catch (error) {
         handleHttpError(res, 'Error_detail_item');//403
     }
@@ -38,28 +41,44 @@ const getItem = async (req, res) => {
  * @param {*} res 
  */
 const createItem = async (req, res) => {
-    const { file } = req;
-    // console.log(file);
-    const fileData = {
-        filename: file.filename,
-        url: `${PUBLIC_URL}/${file.filename}`
-    };
-    const data = await storageModel.create(fileData);
-    res.send({ data });
+    try {
+        const { file } = req;
+        // console.log(file);
+        const fileData = {
+            filename: file.filename,
+            url: `${PUBLIC_URL}/${file.filename}`
+        };
+        const data = await storageModel.create(fileData);
+        res.send({ data });
+    } catch (error) {
+        handleHttpError(res, 'Error_create_item');//403
+    }
 };
-
-/**
- * Actualizar un item en la base de datos
- * @param {*} req 
- * @param {*} res 
- */
-const updateItem = async (req, res) => { };
 
 /**
  * Eliminar un item en la base de datos
  * @param {*} req 
  * @param {*} res 
  */
-const deleteItem = async (req, res) => { };
+const deleteItem = async (req, res) => {
+    try {
+        const { id } = matchedData(req);
+        const dataFile = await storageModel.findById(id);
+        // await storageModel.deleteOne({ _id: id }); // Eliminar de la base de datos
+        await storageModel.delete({ _id: id }); // Soft delete de la base de datos
+        const { filename } = dataFile;
+        const filePath = `${MEDIA_PATH}/${filename}`;
 
-module.exports = { getItems, getItem, createItem, updateItem, deleteItem };
+        // fs.unlinkSync(filePath); // Eliminar archivo del sistema
+        const data = {
+            filePath,
+            deleted: 1
+        };
+
+        res.send({ data });
+    } catch (error) {
+        handleHttpError(res, 'Error_detail_item');//403
+    }
+};
+
+module.exports = { getItems, getItem, createItem, deleteItem };
