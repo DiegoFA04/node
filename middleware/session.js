7cbcb08990ee93f43b197ec063d14e7a0aa52722
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-08-20 16:04:15 -0500

    fix: updated middelware

--- middleware/session.js
+++ middleware/session.js
@@ -1,6 +1,8 @@
 const { handleHttpError } = require('../utils/handleError');
 const { veriftyToken } = require('../utils/handleJwt');
 const { usersModel } = require('../models');
+const getProperties = require('../utils/handlePropertiesEngine');
+const propertiesKey = getProperties();
 
 const authMiddleware = async (req, res, next) => {
     try {
@@ -12,12 +14,16 @@ const authMiddleware = async (req, res, next) => {
         const token = req.headers.authorization.split(' ')[1];
         const dataToken = await veriftyToken(token);
 
-        if (!dataToken._id) {
-            handleHttpError(res, 'ERROR_ID_TOKEN', 401);
+        if (!dataToken) {
+            handleHttpError(res, 'NOT_PAYLOAD_DATA', 401);
             return;
         }
 
-        const user = await usersModel.findById(dataToken._id);
+        const query = {
+            [propertiesKey.id]: dataToken[propertiesKey.id]
+        };
+
+        const user = await usersModel.findOne({});
         req.user = user;
 
         next();
