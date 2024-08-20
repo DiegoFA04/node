a758f710a5ef545e33c12fe93fb11ca95408e296
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-08-20 16:03:55 -0500

    feat: updated const model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
+const ENGINE_DB = process.env.ENGINE_DB;
+
+const pathModels = (ENGINE_DB === 'nosql') ? './nosql' : './mysql';
+
 const models = {
-    usersModel: require('./nosql/users'),
-    storageModel: require('./nosql/storage'),
-    tracksModel: require('./nosql/tracks')
+    usersModel: require(`${pathModels}/users`),
+    storageModel: require(`${pathModels}/storage`),
+    tracksModel: require(`${pathModels}/tracks`),
 }
 
 module.exports = models;
